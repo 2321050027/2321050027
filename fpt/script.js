6474abe5f995991eaeab1d44a0fc6a938cf31ebf
Abcdefghikml <phancantuan73@gmail.com>
2025-11-03 18:35:45 +0700

    Add files via upload

---
+++ fpt/script.js
@@ -0,0 +1,40 @@
+let danhsachphim=[
+    {
+        id:1,
+        tenPhim:"mua do",
+        namPhatHanh:2025,
+        tuoi:16,
+        thoiLuong:2,
+        quocGia:"Viet Nam",
+        poster:"",
+        theoloai:"chieu rap",
+    },
+     {
+        id:2,
+        tenPhim:"conan",
+        namPhatHanh:2025,
+        tuoi:10,
+        thoiLuong:1.5,
+        quocGia:"Nhat Ban",
+        poster:"",
+        theoloai:"Hoat hinh",  
+    },
+     {
+        id:3,
+        tenPhim:"Nguoi dep va quai vat",
+        namPhatHanh:2015,
+        tuoi:16,
+        thoiLuong:2,
+        quocGia:"My",
+        poster:"",
+        theoloai:"chieu rap",
+    },
+];
+let phimHienTai=danhSachPhim[0];
+
+let banner= document.getElementsByClassName('banner')[0];
+ function chonPhim(idPhim){
+for(let i=0;i< danhsachphim.length;i++){
+    
+}
+ }
